972a5f3899b6207ceed779624634f7fa85f99a1a
Aakrati <aakrjain@cisco.com>
2020-02-04 22:00:19 +0530

    Added HelloAPIView

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -15,7 +15,7 @@ class UserProfileManager(BaseUserManager):
           raise ValueError("Users must have an email address")
 
       email=self.normalize_email(email)
-      user=self.model(email=email,name=name)
+      user=self.model(email=email, name=name)
 
       user.set_password(password)
       user.save(using=self._db)
@@ -39,13 +39,13 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
 
   email=models.EmailField(max_length=255,unique=True)
   name =models.CharField(max_length=255)
-  is_active = models.BooleanField(default=True)
+  is_active=models.BooleanField(default=True)
   is_staff = models.BooleanField(default=False)
 
   objects= UserProfileManager()
 
   USERNAME_FIELD='email'
-  REQUIRED_FIELD= ['name']
+  REQUIRED_FIELDS= ['name']
 
   def get_full_name(self):
       """ Retrieve fullname of user"""
